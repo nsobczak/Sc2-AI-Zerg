773857a96c63de435851bf48b98d302318782675
nsobczak <nicolas.sobczak@orange.fr>
2021-10-19 15:39:42 +0200

    Added build hatch + extractor

--- zerg_bot.py
+++ zerg_bot.py
@@ -1,6 +1,7 @@
 import sc2
 from sc2.constants import *
 
+
 class FirstZergBot(sc2.BotAI):
     def __init__(self):
         self.drone_created_counter = 0
@@ -12,6 +13,8 @@ class FirstZergBot(sc2.BotAI):
         await self.distribute_workers()  # in sc2/bot_ai.py
         await self.build_workers()
         await self.build_overlord()
+        await self.expand()
+        await self.build_extractor()
 
     async def build_workers(self):
         larvae = self.units(LARVA)
@@ -22,6 +25,22 @@ class FirstZergBot(sc2.BotAI):
 
     async def build_overlord(self):
         larvae = self.units(LARVA)
-        if self.supply_left < 5 and self.can_afford(OVERLORD) and not self.already_pending(
-                OVERLORD) and larvae.exists:
+        if self.supply_left < 5 and self.can_afford(OVERLORD) \
+                and not self.already_pending(OVERLORD) and larvae.exists:
             await self.do(larvae.random.train(OVERLORD))
+
+    async def expand(self):
+        if self.units(HATCHERY).amount < 3 and self.can_afford(HATCHERY) and not self.already_pending(HATCHERY):
+            await self.expand_now()
+
+    async def build_extractor(self):
+        for hatch in self.units(HATCHERY).ready:
+            vespenes = self.state.vespene_geyser.closer_than(10.0, hatch)
+            for vespene in vespenes:
+                if not self.can_afford(EXTRACTOR):
+                    break
+                worker = self.select_build_worker(vespene.position)
+                if worker is None:
+                    break
+                if not self.units(EXTRACTOR).closer_than(1.0, vespene).exists:
+                    await self.do(worker.build(EXTRACTOR, vespene))
